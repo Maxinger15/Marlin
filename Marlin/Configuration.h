d4e20485bf0d06792492f430a8a34b303b7c7fde
Max Grallinger <kellermeister.maxgrallinger@gmail.com>
2021-06-20 20:49:38 +0200

    bltouch settings angepasst

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -915,7 +915,7 @@
  * The probe replaces the Z-MIN endstop and is used for Z homing.
  * (Automatically enables USE_PROBE_FOR_Z_HOMING.)
  */
-#define Z_MIN_PROBE_USES_Z_MIN_ENDSTOP_PIN
+//#define Z_MIN_PROBE_USES_Z_MIN_ENDSTOP_PIN
 
 // Force the use of the probe for Z-axis homing
 #define USE_PROBE_FOR_Z_HOMING
@@ -935,7 +935,7 @@
  *      - normally-closed switches to GND and D32.
  *      - normally-open switches to 5V and D32.
  */
-//#define Z_MIN_PROBE_PIN 32 // Pin 32 is the RAMPS default
+#define Z_MIN_PROBE_PIN P0_10 // Pin 32 is the RAMPS default
 
 /**
  * Probe Type
@@ -972,7 +972,7 @@
 /**
  * The BLTouch probe uses a Hall effect sensor and emulates a servo.
  */
-5#define BLTOUCH
+#define BLTOUCH
 
 /**
  * Touch-MI Probe by hotends.fr
